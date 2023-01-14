e4b861e41bc01a437f0a79c9c7a09cc9d4329322
JEONG <jungds95@naver.com>
2023-01-14 14:59:15 +0900

    배당금 프로젝트

---
+++ sample1/src/main/java/com/dayone/Application.java
@@ -0,0 +1,28 @@
+package com.dayone;
+
+
+import com.dayone.model.Company;
+import com.dayone.scraper.NaverFinanceScraper;
+import com.dayone.scraper.Scraper;
+import com.dayone.scraper.YahooFinanceScraper;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+//@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(Application.class, args);
+
+        Scraper scraper = new NaverFinanceScraper();
+        var result = scraper.scrapCompanyByTicker("MMM");
+        System.out.println(result);
+    }
+}
