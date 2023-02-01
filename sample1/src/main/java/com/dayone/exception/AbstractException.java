d633a570a4c3315c92520011a3646c513787b12c
JEONG <jungds95@naver.com>
2023-02-01 15:53:33 +0900

    폴더 이동

--- sample1/src/main/java/com/dayone/exception/AbstractException.java
+++
@@ -1,7 +0,0 @@
-package com.dayone.exception;
-
-public abstract class AbstractException extends RuntimeException{
-
-    abstract public int getStatucCode();
-    abstract public String getMessage();
-}
